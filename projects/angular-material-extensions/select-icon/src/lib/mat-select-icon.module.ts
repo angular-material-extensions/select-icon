d9a7dfa8e3e8775dde27dde1629c433194272699
Anthony Nahas <anthony.na@hotmail.de>
2020-10-05 23:48:20 +0200

    fix(lib): enhanced the import statement of the lib module

--- projects/angular-material-extensions/select-icon/src/lib/mat-select-icon.module.ts
+++ projects/angular-material-extensions/select-icon/src/lib/mat-select-icon.module.ts
@@ -1,10 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
 import { MatSelectIconComponent } from './mat-select-icon/mat-select-icon.component';
 
 
 @NgModule({
   declarations: [MatSelectIconComponent],
-  imports: [],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    FlexLayoutModule
+  ],
   exports: [MatSelectIconComponent]
 })
 export class MatSelectIconModule {
